c072df4a47b5f37be574a023bf24e58c2dca7a2d
Jhaschen <j.aschen@ostfalia.de>
2024-04-10 11:02:28 +0000

    + non blocking Bespiel

--- main.cpp
+++ main.cpp
@@ -24,33 +24,58 @@ Thread thread3;
 
 void send_thread(void)
 {
+    
     int message = 0;
-  
-
+    int* pmessage= &message;
     while (true) {
             ThisThread::sleep_for(200ms);
              message++; // counter
-            queue.try_put(&message);//Put counter to message queue
+            
+            // blocking
+            //queue.put(pmessage);//Put counter to message queue
+            //Non blocking
+            while(!(queue.try_put(pmessage))){
+               ThisThread::sleep_for(10ms);
+           }
         }
     
 }
 
 void recieve_thread(void)
 {
+     int *message;
     while (true) {
+
+
+        // blocking
+        /*
         osEvent evt = queue.get(); //recieve message from queue
         if (evt.status == osEventMessage) { //check if message available
             int *message = (int*)evt.value.p; //copy message
             if(*message % 3 == 0){  //if counter is multiple of 3, then toggle LED 2 
                 led2 = !led2;
             }
-            queue2.try_put(message); //Put counter to message queue2
+            queue2.put(message); //Put counter to message queue2
+            */
+            // Non blocking
+           while(!(queue.try_get(&message))){
+               ThisThread::sleep_for(10ms);
+           }
+            if(*message % 3 == 0){  //if counter is multiple of 3, then toggle LED 2 
+                led2 = !led2;
+            }
+            while(!(queue2.try_put(message))){
+               ThisThread::sleep_for(10ms);
+           }
+
+            
         }
     }
-}
+
 void recieve_thread2(void)
 {
     while (true) {
+        // blocking
         osEvent evt = queue2.get(); //recieve message from queue
         if (evt.status == osEventMessage) { //check if message available
             int *message = (int*)evt.value.p; //copy message
